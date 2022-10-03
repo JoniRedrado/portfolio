796d8d1b888da4fccf54ac3a5cff9ac1b0e69a40
JonathanRedrado <jonathan.redrado98@gmail.com>
2022-10-03 18:20:18 -0300

    Upgrades V2

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -1,16 +1,19 @@
 import React from "react";
 import './Slider.css'
 
+
 const slidesInfo = [
     {
-        src: "https://cdn.pixabay.com/photo/2016/12/19/08/39/mobile-phone-1917737_960_720.jpg",
+        src: "https://i.ibb.co/r0QQ7GS/tateti.png",
         alt:"Project 1",
-        desc:"TA-TE-TI"
+        desc:"TA-TE-TI",
+        link:"https://tatetijr.netlify.app"
     },
     {
-        src: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
+        src: "https://i.ibb.co/tM16YtJ/countries-wiki.png",
         alt:"Project 2",
-        desc:"Countries API"
+        desc:"Countries Wiki",
+        link:"https://countrieswiki.herokuapp.com"
     },
     {
         src: "https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg",
@@ -20,12 +23,14 @@ const slidesInfo = [
 ]
 
 const Slides = slidesInfo.map( slide => (
-    <div className="slide-container">
-        <img src={slide.src} alt={slide.alt} />
-        <div className="slide-desc">
-            <span>{slide.desc}</span>
-        </div>
-    </div>
+    
+        <a href={slide.link} target="_blank" rel="noreferrer" className="slide-container">
+            <img src={slide.src} alt={slide.alt} />
+            <div className="slide-desc">
+                <span>{slide.desc}</span>
+            </div>
+        </a>
+    
 ))
 
 export default Slides;
